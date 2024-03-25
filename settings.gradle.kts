f76ea3bd57301cd110465da3472293b3fd653389
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2024-03-25 17:45:52 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            name = "arcverseRepository"
+            url = uri("https://repo.arcver.se/private")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+}
+
+rootProject.name = "Calculator"
+include(":app")
