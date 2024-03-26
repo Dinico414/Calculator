cf93aab1c20d91ff1d05c659f9b243e1abf25293
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2024-03-26 12:01:08 +0100

    release build in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,12 +23,16 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
