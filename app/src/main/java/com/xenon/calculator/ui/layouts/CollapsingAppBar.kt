0e9ae5d09ba77ca69f53647bc7b70eb2ba4666ff
Dinico414 <nico.broese.iq@gmail.com>
2025-05-24 14:44:18 +0200

    Add Color change to collapsing toolbar

--- app/src/main/java/com/xenon/calculator/ui/layouts/CollapsingAppBar.kt
+++ app/src/main/java/com/xenon/calculator/ui/layouts/CollapsingAppBar.kt
@@ -7,14 +7,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -27,6 +32,11 @@ fun CollapsingAppBarLayout(
     collapsedHeight: Dp = 54.dp,
     expandedHeight: Dp = 200.dp,
     navigationIcon: @Composable () -> Unit = {},
+    expandedTextColor: Color = MaterialTheme.colorScheme.primary,
+    collapsedTextColor: Color = MaterialTheme.colorScheme.onBackground,
+    expandedContainerColor: Color = MaterialTheme.colorScheme.background,
+    collapsedContainerColor: Color = MaterialTheme.colorScheme.background,
+    navigationIconContentColor: Color = MaterialTheme.colorScheme.onBackground,
     content: @Composable (paddingValues: PaddingValues) -> Unit
 ) {
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
@@ -37,19 +47,25 @@ fun CollapsingAppBarLayout(
                 title = {},
                 collapsedHeight = collapsedHeight,
                 expandedHeight = expandedHeight,
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Color.Transparent,
-                    scrolledContainerColor = Color.Transparent,
-                    navigationIconContentColor = Color.Transparent,
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = expandedContainerColor,
+                    scrolledContainerColor = collapsedContainerColor,
+                    navigationIconContentColor = navigationIconContentColor,
                     titleContentColor = Color.Transparent,
-                    actionIconContentColor = Color.Transparent,
+                    actionIconContentColor = navigationIconContentColor,
                 ),
                 scrollBehavior = scrollBehavior
             )
 
             val fraction = scrollBehavior.state.collapsedFraction
-            val curHeight = collapsedHeight.times(fraction) + expandedHeight.times(1-fraction)
-            val curFontSize = (24*fraction + 45*(1-fraction)).sp
+            val curHeight by remember(fraction) { derivedStateOf { collapsedHeight.times(fraction) + expandedHeight.times(1 - fraction) } }
+            val curFontSize by remember(fraction) { derivedStateOf { (24 * fraction + 45 * (1 - fraction)).sp } }
+
+            val interpolatedTextColor by remember(fraction, expandedTextColor, collapsedTextColor) {
+                derivedStateOf {
+                    lerp(expandedTextColor, collapsedTextColor, fraction)
+                }
+            }
 
             CenterAlignedTopAppBar(
                 expandedHeight = curHeight,
@@ -59,7 +75,7 @@ fun CollapsingAppBarLayout(
                             .fillMaxHeight(),
                         contentAlignment = Alignment.Center
                     ) {
-                        Text("Settings", fontSize = curFontSize)
+                        Text("Settings", fontSize = curFontSize, color = interpolatedTextColor)
                     }
                 },
                 navigationIcon = {
@@ -74,7 +90,10 @@ fun CollapsingAppBarLayout(
                 },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = Color.Transparent,
-                    scrolledContainerColor = Color.Transparent
+                    scrolledContainerColor = Color.Transparent,
+                    navigationIconContentColor = navigationIconContentColor,
+                    titleContentColor = interpolatedTextColor,
+                    actionIconContentColor = navigationIconContentColor
                 ),
             )
         }
---
+++ app/src/main/java/com/xenon/calculator/ui/layouts/converter/CompactConverter.kt
@@ -0,0 +1,4 @@
+package com.xenon.calculator.ui.layouts.converter
+
+class CompactConverter {
+}
