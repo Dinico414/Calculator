db67069320f9d4aae48361459d1e548b43bb0727
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-06-11 12:37:48 +0200

    make coverscreenlayout rotateable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 5
-        versionName = "1.7.3"
+        versionName = "1.7.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
