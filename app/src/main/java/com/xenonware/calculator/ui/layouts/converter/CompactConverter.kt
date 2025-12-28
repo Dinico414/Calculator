de12be306cbae5e6693969f80a9bdc10040c9a42
Xenon-Ware <116182930+Dinico414@users.noreply.github.com>
2025-12-28 21:38:08 +0100

    make , or . input to . and block out letters

--- app/src/main/java/com/xenonware/calculator/ui/layouts/converter/CompactConverter.kt
+++ app/src/main/java/com/xenonware/calculator/ui/layouts/converter/CompactConverter.kt
@@ -1,5 +1,6 @@
 package com.xenonware.calculator.ui.layouts.converter
 
+import android.annotation.SuppressLint
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
@@ -63,6 +64,7 @@ import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.hazeSource
 import dev.chrisbanes.haze.materials.ExperimentalHazeMaterialsApi
 
+@SuppressLint("ConfigurationScreenWidthHeight")
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalHazeMaterialsApi::class)
 @Composable
 fun CompactConverter(
@@ -205,10 +207,13 @@ fun CompactConverter(
                     )
                     XenonTextField(
                         value = value1,
-                        onValueChange = { newValue ->
-                            viewModel.onValueChanged(
-                                newValue, ConverterViewModel.EditedField.FIELD1
-                            )
+                        onValueChange = { incoming ->
+                            val processed = incoming.replace(',', '.')
+
+                            val regex = Regex("^-?\\d*\\.?\\d*$")
+                            if (processed.matches(regex)) {
+                                viewModel.onValueChanged(processed, ConverterViewModel.EditedField.FIELD1)
+                            }
                         },
                         placeholder = {Text(stringResource(id = R.string.value_1))},
                         singleLine = true,
@@ -284,10 +289,13 @@ fun CompactConverter(
                     )
                     XenonTextField(
                         value = value2,
-                        onValueChange = { newValue ->
-                            viewModel.onValueChanged(
-                                newValue, ConverterViewModel.EditedField.FIELD2
-                            )
+                        onValueChange = { incoming ->
+                            val processed = incoming.replace(',', '.')
+
+                            val regex = Regex("^-?\\d*\\.?\\d*$")
+                            if (processed.matches(regex)) {
+                                viewModel.onValueChanged(processed, ConverterViewModel.EditedField.FIELD2)
+                            }
                         },
                         placeholder = {Text(stringResource(id = R.string.value_2))},
                         singleLine = true,
